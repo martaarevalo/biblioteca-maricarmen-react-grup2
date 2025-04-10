b60a97ffb2204ce834aefa36f4c48533742d0fa6
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-04-10 11:28:04 +0200

    [2 - login usuari - DONE(?)] - permite editar el ciclo el teléfono y el centro

--- src/components/ProfilePanel.jsx
+++ src/components/ProfilePanel.jsx
@@ -70,8 +70,8 @@ export function ProfilePanel({
                   <span className="profile-field-label">Teléfono</span>
                   <input
                     type="text"
-                    name="phone"
-                    value={editedDetails.phone || ""}
+                    name="telefon"
+                    value={editedDetails.telefon || ""}
                     onChange={handleInputChange}
                     className="profile-input"
                   />
@@ -82,8 +82,8 @@ export function ProfilePanel({
                   <span className="profile-field-label">Centro</span>
                   <input
                     type="text"
-                    name="center"
-                    value={editedDetails.center || ""}
+                    name="centre"
+                    value={editedDetails.centre || ""}
                     onChange={handleInputChange}
                     className="profile-input"
                   />
@@ -92,8 +92,8 @@ export function ProfilePanel({
                   <span className="profile-field-label">Ciclo</span>
                   <input
                     type="text"
-                    name="cycle"
-                    value={editedDetails.cycle || ""}
+                    name="cicle"
+                    value={editedDetails.cicle || ""}
                     onChange={handleInputChange}
                     className="profile-input"
                   />
@@ -154,7 +154,7 @@ export function ProfilePanel({
                 <div className="profile-field">
                   <span className="profile-field-label">Teléfono</span>
                   <div className="profile-field-value">
-                    {currentDetails.phone || "No especificado"}
+                    {currentDetails.telefon || "No especificado"}
                   </div>
                 </div>
               </div>
@@ -162,13 +162,13 @@ export function ProfilePanel({
                 <div className="profile-field">
                   <span className="profile-field-label">Centro</span>
                   <div className="profile-field-value">
-                    {currentDetails.center || "No especificado"}
+                    {currentDetails.centre || "No especificado"}
                   </div>
                 </div>
                 <div className="profile-field">
                   <span className="profile-field-label">Ciclo</span>
                   <div className="profile-field-value">
-                    {currentDetails.cycle || "No especificado"}
+                    {currentDetails.cicle || "No especificado"}
                   </div>
                 </div>
               </div>
